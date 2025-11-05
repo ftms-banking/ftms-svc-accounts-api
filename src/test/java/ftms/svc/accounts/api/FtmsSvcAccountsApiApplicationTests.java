0afd1e6ef9c505b992550a468fecd1ee4ee82a6a
Avadhut Shinde <72093748+rootAvadhut@users.noreply.github.com>
2025-11-05 18:20:48 +0530

    feat: add db env props for test phase

--- src/test/java/ftms/svc/accounts/api/FtmsSvcAccountsApiApplicationTests.java
+++ src/test/java/ftms/svc/accounts/api/FtmsSvcAccountsApiApplicationTests.java
@@ -2,10 +2,12 @@ package ftms.svc.accounts.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FtmsSvcAccountsApiApplicationTests {
 
 	@Test
